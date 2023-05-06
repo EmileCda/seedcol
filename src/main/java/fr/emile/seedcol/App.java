678d84128acfcf9a15c46b6429c1e18f817ae427
emileCda <emile.cda@gmail.com>
2023-05-06 11:01:25 +0200

    Evironment project seedcol : ready

---
+++ src/main/java/fr/emile/seedcol/App.java
@@ -0,0 +1,13 @@
+package fr.emile.seedcol;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
